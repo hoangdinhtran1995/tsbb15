d80392de7dd4064096c12961618aa54dec036197
Hoang Tran <51941188+hoangdinhtran1995@users.noreply.github.com>
2021-02-04 11:38:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import lab1
 from regularizedderivatives import regularized_values
 from estimateD import estimate_d
 from interpolate import interpolate
+from orientationTensor import orientation_tensor
 
 I, J, dTrue = lab1.get_cameraman()
 
@@ -18,46 +19,11 @@ dtot = estimate_d(I, J, 120, 85, (40, 70), max_iterations=10 , min_error=0)
 print('dTrue = ', dTrue)
 print('dtot = ', (dtot[0][0],dtot[1][0]))
 
-# plt.figure(1)
-# plt.imshow(I)
-# plt.figure(2)
-# plt.imshow(J)
-#
-#
-# print('dTrue =', dTrue)
-#
-# Ig, Jg, Jgdx, Jgdy = regularized_values(I, J, 6, 1)
-# plt.figure(3)
-# plt.imshow(Ig)
-# plt.figure(4)
-# plt.imshow(Jg)
-# plt.figure(5)
-# plt.imshow(Jgdx)
-# plt.figure(6)
-# plt.imshow(Jgdy)
-
-
-
-#testim = mpimage.imread('F:/kurser/master/tsbb15/CE/TSBB15/images/boat.png').astype('float32') / 255
-
-#test of rectbivarspline
-#y = 300
-#x = 300
-#width = 20
-#height = 30
-#testim_interp = interpolate(testim, y, x, height, width)
-#
-#x2 = np.arange(x, x+width, 0.25)
-#y2 = np.arange(y, y+height, 0.25)
-#tmp = testim_interp(y2, x2)
-
-
-#plt.figure(7)
-#plt.imshow(tmp, cmap = 'gray')
-#plt.figure(8)
-#plt.imshow(testim[y:y+height,x:x+width], cmap = 'gray')
-#
-#plt.show()
-
-
-
+T_field = orientation_tensor(I, 5, 0.5)
+plt.figure(1)
+plt.imshow(T_field[:,:,1])
+plt.figure(2)
+plt.imshow(T_field[:,:,2])
+plt.figure(3)
+plt.imshow(T_field[:,:,0])
+plt.show()
--- orientationTensor.py
+++ orientationTensor.py
@@ -2,7 +2,9 @@ import numpy as np
 from scipy.signal import convolve2d as conv2
 
 
-def orientation_tensor(img, grad_ksize, grad_sigma, ksize, sigma):
+def orientation_tensor(img, ksize, sigma):
+    # returns img with tensors in 3rd dim, T11, T12, T22 respectively
+
     lp = np.atleast_2d(np.exp(-0.5 * (np.arange(-ksize, ksize + 1) / sigma) ** 2))
     lp = lp / np.sum(lp)  # normalize the filter
     df = np.atleast_2d(-1.0 / np.square(sigma) * np.arange(-ksize, ksize + 1) * lp) # 1d deriv filt
@@ -10,6 +12,8 @@ def orientation_tensor(img, grad_ksize, grad_sigma, ksize, sigma):
     dx = conv2(img, df, mode = "same")
     dy = conv2(img, df.T, mode = "same")
 
-    T_field = np.empty(np.shape(img))
-
+    T_field = np.zeros((np.shape(img)[0], np.shape(img)[1], 3))
+    T_field[:, :, 0] = dx * dx
+    T_field[:, :, 1] = dx * dy
+    T_field[:, :, 2] = dy * dy
     return T_field
