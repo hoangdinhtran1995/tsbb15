1359681f28740a7fb26aa2456457a12acad6cf39
Hoang Tran <51941188+hoangdinhtran1995@users.noreply.github.com>
2021-02-04 23:03:07 +0100

    Add files via upload

--- harris.py
+++ harris.py
@@ -1,2 +1,14 @@
+import numpy as np
 
+def harris(kappa, T_field):
+    # takes in T_field as a img where each pixel has T11, T12, T22 respectively in 3rd dim
 
+    H_field = np.zeros((np.shape(T_field)[0], np.shape(T_field)[1]))
+    for row in range(np.shape(T_field)[0]):
+        for col in range(np.shape(T_field)[1]):
+            T11 = T_field[row, col, 0]
+            T12 = T_field[row, col, 1]
+            T22 = T_field[row, col, 2]
+            H_field[row, col] = T11 * T22 - T12 * T12 - kappa * (T11 + T22)**2
+
+    return H_field
