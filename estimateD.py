726057d69e45d6be472a7b45741ed56d9666e7c8
Hoang Tran <51941188+hoangdinhtran1995@users.noreply.github.com>
2021-02-04 23:03:57 +0100

    lab1 finish
    lab1 final

--- estimateD.py
+++ estimateD.py
@@ -17,14 +17,12 @@ def estimate_d(I, J, x, y, window_size, max_iterations=50, min_error=0):
     dtot = np.zeros((2, 1))
     im_width = np.shape(J)[1]
     im_height = np.shape(J)[0]
-    xcoords = np.arange(0, im_width)
-    ycoords = np.arange(0, im_height)
 
-    Ig, Jg, Jgdx, Jgdy = regularized_values(I, J, 5, 1)
+    Ig, Jg, Jgdx, Jgdy = regularized_values(I, J, 10, 1)
 
-    Jg_og = Jg
-    Jgdx_og = Jgdx
-    Jgdy_og = Jgdy
+    Jg_og = np.copy(Jg)
+    Jgdx_og = np.copy(Jgdx)
+    Jgdy_og = np.copy(Jgdy)
 
     for tmp in range(max_iterations):
 
@@ -36,11 +34,11 @@ def estimate_d(I, J, x, y, window_size, max_iterations=50, min_error=0):
             break
 
         Jg_interpol = interpolate(Jg_og, 0, 0, im_height, im_width)
-        Jg = Jg_interpol(np.arange(dtot[1], im_height + dtot[1]), np.arange(dtot[0], im_width + dtot[0]), grid=True)
+        Jg = Jg_interpol(np.arange(dtot[1], im_height + dtot[1]), np.arange(dtot[0], im_width + dtot[0]))
 
         Jgdx_interpol = interpolate(Jgdx_og, 0, 0, im_height, im_width)
-        Jgdx = Jgdx_interpol(np.arange(dtot[1], im_height + dtot[1]), np.arange(dtot[0], im_width + dtot[0]), grid=True)
+        Jgdx = Jgdx_interpol(np.arange(dtot[1], im_height + dtot[1]), np.arange(dtot[0], im_width + dtot[0]))
 
         Jgdy_interpol = interpolate(Jgdy_og, 0, 0, im_height, im_width)
-        Jgdy = Jgdy_interpol(np.arange(dtot[1], im_height + dtot[1]), np.arange(dtot[0], im_width + dtot[0]), grid=True)
+        Jgdy = Jgdy_interpol(np.arange(dtot[1], im_height + dtot[1]), np.arange(dtot[0], im_width + dtot[0]))
     return dtot
